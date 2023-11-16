cdf025721a0f6f4d1ce13fb9943507af45383b86
Dominik Ziolkowski <minickisbeast@gmail.com>
2023-11-16 02:16:16 -0500

    Another work in progress done
    Added the button across all pages for the collapse and expand for the side bar, added the proper styling across each page, need to fix alot of bugs and eventually redo how we do the button cause its kind of awkward and disgusting. Made some colour changes to the ui (dark mode specifically), making good progress and moving efficiently but alot left to do.

--- src/main/resources/static/javascript/SideBar_View_Dropdown.js
+++ src/main/resources/static/javascript/SideBar_View_Dropdown.js
@@ -1,4 +1,12 @@
 $(() => {
+	
+	//Do this first time irregardless of click at least once.
+	let offset = -202.703;
+		console.log("Total offset: " + ($(".SideBarContainer").outerWidth() + offset) + "px");	
+		
+		$(".ButtonAndContentWrapper").css("left", ($(".SideBarContainer").outerWidth() + offset) + "px");
+		
+	
 	//For displaying the accordion style view links
 	let i;
 	for (i = 0; i < $(".accordion").length; i++) {
@@ -24,6 +32,14 @@ $(() => {
 	//For opening and closing the side bar
 	$("#OpenCloseBtn").click(() => {
 		$(".SideBarLinkText, footer").toggle();
+		
+		console.log($(".SideBarContainer").outerWidth());	
+		
+		let offset = -202.703;
+		console.log("Total offset: " + ($(".SideBarContainer").outerWidth() + offset) + "px");	
+		
+		$(".ButtonAndContentWrapper").css("left", ($(".SideBarContainer").outerWidth() + offset) + "px");
+		
 		if ($(".SideBarLinkText").css("display") === "none") {
 			if ($(".panel").css("display") === "flex") {
 				$(".ViewInnerContainer").css("background-color", "gray");
@@ -34,11 +50,15 @@ $(() => {
 			}
 			$(".SideBarIcon").css("padding", "0px 5px 0px");
 			$(".panel").css("padding", "0px 0px 0px");
+			$(".SideBarBottomArea").css("border-top", "0px solid white");
 		} else {
 			$(".ViewInnerContainer").css("background-color", "");
 			$(".ViewInnerContainer").css("border-radius", "0px");
 			$(".SideBarIcon").css("padding", "0px 0px 0px 5px");
 			$(".panel").css("padding", "0px 0px 0px 30px");
+			$(".SideBarBottomArea").css("border-top", "1px solid #2e2e32");
 		}
 	});
 });
+
+	
