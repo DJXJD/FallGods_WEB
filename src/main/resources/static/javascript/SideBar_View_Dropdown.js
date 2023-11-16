bb52c26a0317d3086a7bb65e58bf236f47845d2e
Dominik Ziolkowski <minickisbeast@gmail.com>
2023-11-15 21:07:16 -0500

    Another UI Overhaul WIP
    Added the collapse of the view to be lined up correctly, and have it display a nice background when the sidebar is collapsed.

--- src/main/resources/static/javascript/SideBar_View_Dropdown.js
+++ src/main/resources/static/javascript/SideBar_View_Dropdown.js
@@ -1,22 +1,44 @@
 $(() => {
-	let acc = $(".accordion");
-
 	//For displaying the accordion style view links
-	var i;
-	for (i = 0; i < acc.length; i++) {
-		acc[i].addEventListener("click", function() {
-			var panel = this.nextElementSibling;
-			if (panel.style.display === "block") {
-				panel.style.display = "none";
+	let i;
+	for (i = 0; i < $(".accordion").length; i++) {
+		$(".accordion")[i].addEventListener("click", function() {
+			if (this.nextElementSibling.style.display === "flex") {
+				this.nextElementSibling.style.display = "none";
+				$(".ViewInnerContainer").css("background-color", "");
+				$(".ViewInnerContainer").css("border-radius", "0px");
 			} else {
-				panel.style.display = "block";
+				if ($(".SideBarLinkText").css("display") === "none") {
+					$(".ViewInnerContainer").css("background-color", "gray");
+					$(".ViewInnerContainer").css("border-radius", "5px");
+				} else {
+					$(".ViewInnerContainer").css("background-color", "");
+					$(".ViewInnerContainer").css("border-radius", "0px");
+				}
+				this.nextElementSibling.style.display = "flex";
+				this.nextElementSibling.style.flexDirection = "column";
 			}
 		});
 	}
 
 	//For opening and closing the side bar
-	let IsOpen = true;
 	$("#OpenCloseBtn").click(() => {
 		$(".SideBarLinkText, footer").toggle();
+		if ($(".SideBarLinkText").css("display") === "none") {
+			if ($(".panel").css("display") === "flex") {
+				$(".ViewInnerContainer").css("background-color", "gray");
+				$(".ViewInnerContainer").css("border-radius", "5px");
+			} else {
+				$(".ViewInnerContainer").css("background-color", "");
+				$(".ViewInnerContainer").css("border-radius", "0px");
+			}
+			$(".SideBarIcon").css("padding", "0px 5px 0px");
+			$(".panel").css("padding", "0px 0px 0px");
+		} else {
+			$(".ViewInnerContainer").css("background-color", "");
+			$(".ViewInnerContainer").css("border-radius", "0px");
+			$(".SideBarIcon").css("padding", "0px 0px 0px 5px");
+			$(".panel").css("padding", "0px 0px 0px 30px");
+		}
 	});
 });
