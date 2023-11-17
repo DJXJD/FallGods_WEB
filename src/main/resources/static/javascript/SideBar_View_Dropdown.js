cd57a49219dd2223c99deb3293f054fa221ca396
Dominik Ziolkowski <minickisbeast@gmail.com>
2023-11-16 21:09:24 -0500

    UI Overhaul still in progress
    Got the ability to keep the sidebar and accordion the correct state when going through pages.
    Fixed a issue when clicking on the sidebar link and switching pages, the page would reload, and set it to the original design for the sidebar, causing a small glitch, this is now fixed and is loading correctly with no weird visual things, fragmeneted the code across all the files, adding in a better maincontent area which will help keep things more centered. Changed the colours of the text and keep the page that is currently loaded highlighted in the sidebar so we can visually see where we are.

--- src/main/resources/static/javascript/SideBar_View_Dropdown.js
+++ src/main/resources/static/javascript/SideBar_View_Dropdown.js
@@ -1,64 +1,116 @@
 $(() => {
-	
-	//Do this first time irregardless of click at least once.
-	let offset = -202.703;
-		console.log("Total offset: " + ($(".SideBarContainer").outerWidth() + offset) + "px");	
-		
-		$(".ButtonAndContentWrapper").css("left", ($(".SideBarContainer").outerWidth() + offset) + "px");
-		
-	
+	//Check to see if the accordion and sidebar should be collapsed or open on load.
+	CheckSideBarStatus();
+	CheckAccordionViewStatus();
+
 	//For displaying the accordion style view links
-	let i;
-	for (i = 0; i < $(".accordion").length; i++) {
-		$(".accordion")[i].addEventListener("click", function() {
-			if (this.nextElementSibling.style.display === "flex") {
-				this.nextElementSibling.style.display = "none";
-				$(".ViewInnerContainer").css("background-color", "");
-				$(".ViewInnerContainer").css("border-radius", "0px");
-			} else {
-				if ($(".SideBarLinkText").css("display") === "none") {
-					$(".ViewInnerContainer").css("background-color", "gray");
-					$(".ViewInnerContainer").css("border-radius", "5px");
-				} else {
-					$(".ViewInnerContainer").css("background-color", "");
-					$(".ViewInnerContainer").css("border-radius", "0px");
-				}
-				this.nextElementSibling.style.display = "flex";
-				this.nextElementSibling.style.flexDirection = "column";
-			}
-		});
-	}
+	$(".accordion").on("click", function() {
+		if ($(".accordion").next().css("display") === "flex") {
+			SetAccordionView("none");
+		} else if ($(".accordion").next().css("display") === "none") {
+			SetAccordionView("flex");
+		}
+	});
 
 	//For opening and closing the side bar
 	$("#OpenCloseBtn").click(() => {
-		$(".SideBarLinkText, footer").toggle();
-		
-		console.log($(".SideBarContainer").outerWidth());	
-		
-		let offset = -202.703;
-		console.log("Total offset: " + ($(".SideBarContainer").outerWidth() + offset) + "px");	
-		
-		$(".ButtonAndContentWrapper").css("left", ($(".SideBarContainer").outerWidth() + offset) + "px");
-		
-		if ($(".SideBarLinkText").css("display") === "none") {
-			if ($(".panel").css("display") === "flex") {
+		if (localStorage.getItem("SideBar") === "none") {
+			SetSideBarView("flex");
+		} else {
+			SetSideBarView("none");
+		}
+	});
+
+	//Check the sidebar status to set to on load
+	function CheckSideBarStatus() {
+		if (localStorage.getItem("SideBar") === "flex") {
+			SetSideBarView("flex");
+		} else if (localStorage.getItem("SideBar") === "none") {
+			SetSideBarView("none");
+		}
+	}
+
+	//Check the accordion view menu status to set to on load
+	function CheckAccordionViewStatus() {
+		if (localStorage.getItem("AccordionView") === "flex") {
+			SetAccordionView("flex");
+		} else if (localStorage.getItem("AccordionView") === "none") {
+			SetAccordionView("none");
+		}
+	}
+
+	//Set the accordion view properties for opening and closing.
+	function SetAccordionView(displayType) {
+		if (displayType === "none") {
+			localStorage.setItem("AccordionView", "none");
+			$(".accordion").next().css("display", "none");
+			$(".ViewInnerContainer").css("background-color", "");
+			$(".ViewInnerContainer").css("border-radius", "0px");
+		} else if (displayType === "flex") {
+			localStorage.setItem("AccordionView", "flex");
+			if ($(".SideBarLinkText").css("display") === "none") {
 				$(".ViewInnerContainer").css("background-color", "gray");
 				$(".ViewInnerContainer").css("border-radius", "5px");
 			} else {
 				$(".ViewInnerContainer").css("background-color", "");
 				$(".ViewInnerContainer").css("border-radius", "0px");
 			}
-			$(".SideBarIcon").css("padding", "0px 5px 0px");
-			$(".panel").css("padding", "0px 0px 0px");
-			$(".SideBarBottomArea").css("border-top", "0px solid white");
-		} else {
+
+			$(".accordion").next().css("display", "flex");
+			$(".accordion").next().css("flex-direction", "column");
+		}
+	}
+	//Set the sidebarview properties for opening and closing.
+	function SetSideBarView(displayType) {
+		if (displayType === "none") {
+			localStorage.setItem("SideBar", "none");
+			$(".SideBarLinkText, footer").hide();
 			$(".ViewInnerContainer").css("background-color", "");
 			$(".ViewInnerContainer").css("border-radius", "0px");
-			$(".SideBarIcon").css("padding", "0px 0px 0px 5px");
-			$(".panel").css("padding", "0px 0px 0px 30px");
+			$(".SideBarIcon").css("padding", "0px 5px 0px");
+			$(".panel").css("padding", "0px 0px 0px");
+			if ($(".panel").css("display") === "flex") {
+				$(".ViewInnerContainer").css("background-color", "gray");
+				$(".ViewInnerContainer").css("border-radius", "5px");
+			}
 			$(".SideBarBottomArea").css("border-top", "1px solid #2e2e32");
+		} else if (displayType === "flex") {
+			localStorage.setItem("SideBar", "flex");
+			$(".SideBarLinkText, footer").show();
+			if ($(".panel").css("display") === "flex") {
+				$(".ViewInnerContainer").css("background-color", "");
+				$(".ViewInnerContainer").css("border-radius", "");
+			}
+			$(".SideBarIcon").css("padding", "0px 0px 0px 0px");
+			$(".panel").css("padding", "0px 0px 0px 30px");
+			$(".SideBarBottomArea").css("border-top", "0px solid white");
 		}
+
+		//When button is pressed, we need to make sure we reset the button position
+		let offset = -202.703;
+		$(".ButtonAndContentWrapper").css("left", ($(".SideBarContainer").outerWidth() + offset) + "px");
+	}
+
+	let currentPath = $(location).attr("pathname");
+	//Whenever a link is clicked, this makes sure that it stays lit up, to know where in the navigation we are.
+	$(".IconTextHContainer").each(function(index, element) {
+		if ($(element).closest(".SideBarNavLinks").attr("href") === currentPath) {
+			$(element).addClass("active-link");
+			if ($(element).hasClass("AccordionLink")) {
+				$("#ViewLink").addClass("active-link");
+			}
+		}
+	});
+
+	//When the website is done loading all the stuff, and everything has been applied and calculated
+	//we want to then remove the hidden stuff.
+	$(window).on("load", function() {
+		if ($(".ContainerForAll").hasClass("IsHidden")) {
+			$(".ContainerForAll").removeClass("IsHidden");
+		}
+		//Reset the button here as well to be in the appropriate spot.
+		let offset = -202.703;
+		$(".ButtonAndContentWrapper").css("left", ($(".SideBarContainer").outerWidth() + offset) + "px");
 	});
 });
 
-	
