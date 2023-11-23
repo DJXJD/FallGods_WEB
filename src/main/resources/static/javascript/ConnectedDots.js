a83ff2b932dfe3caad6b068d01ab343dcd6d2c10
Dominik Ziolkowski <minickisbeast@gmail.com>
2023-11-22 21:29:20 -0500

    Fixed the background dots
    Fixed the dots to make them glow, and adjust correctly with screen sizes.

--- src/main/resources/static/javascript/ConnectedDots.js
+++ src/main/resources/static/javascript/ConnectedDots.js
@@ -1,105 +1,143 @@
 var canvas = document.getElementById("canvas"),
-	ctx = canvas.getContext('2d');
+  ctx = canvas.getContext('2d');
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 var stars = [],
-	FPS = 60,
-	x = 100,
-	mouse = {
-	};
+  FPS = 60,
+  x = calculateNumStars(), // Calculate the initial number of stars based on the canvas size
+  mouse = {};
+
+function calculateNumStars() {
+  // Calculate the number of stars based on the canvas size
+  var numStars = Math.floor((canvas.width * canvas.height) / 25000); // Adjust the divisor as needed
+  return numStars;
+}
 
 // Initialize stars
 for (var i = 0; i < x; i++) {
-	stars.push({
-		x: Math.random() * canvas.width,
-		y: Math.random() * canvas.height,
-		radius: Math.random() * (canvas.width / 400) + 1,
-		vx: Math.floor(Math.random() * 50) - 25,
-		vy: Math.floor(Math.random() * 50) - 25
-	});
+  stars.push({
+    x: Math.random() * canvas.width,
+    y: Math.random() * canvas.height,
+    radius: Math.random() * (canvas.width / 400) + 1,
+    vx: Math.floor(Math.random() * 50) - 25,
+    vy: Math.floor(Math.random() * 50) - 25
+  });
 }
 
 function resizeCanvas() {
-	canvas.width = window.innerWidth;
-	canvas.height = window.innerHeight;
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  adjustStarPositions();
+}
+
+function adjustStarPositions() {
+  for (var i = 0, x = stars.length; i < x; i++) {
+    var s = stars[i];
+    s.x = Math.random() * canvas.width;
+    s.y = Math.random() * canvas.height;
+  }
 }
 
 window.addEventListener('resize', function() {
-	resizeCanvas();
-
-	for (var i = 0; i < stars.length; i++) {
-		var s = stars[i];
-		s.x = Math.random() * canvas.width;
-		s.y = Math.random() * canvas.height;
-	}
+  resizeCanvas();
+  var newNumStars = calculateNumStars();
+  
+  if (newNumStars < stars.length) {
+    stars.splice(newNumStars); // Remove excess stars if there are too many
+  } else {
+    // Add or update stars if the number increased or the array is empty (optional)
+    for (var i = stars.length; i < newNumStars; i++) {
+      if (i < stars.length) {
+        // Update existing star's position if the array is not empty
+        stars[i] = {
+          x: Math.random() * canvas.width,
+          y: Math.random() * canvas.height,
+          radius: Math.random() * (canvas.width / 400) + 1,
+          vx: Math.floor(Math.random() * 50) - 25,
+          vy: Math.floor(Math.random() * 50) - 25
+        };
+      } else {
+        // Add new stars if the number increased (array is empty)
+        stars.push({
+          x: Math.random() * canvas.width,
+          y: Math.random() * canvas.height,
+          radius: Math.random() * (canvas.width / 400) + 1,
+          vx: Math.floor(Math.random() * 50) - 25,
+          vy: Math.floor(Math.random() * 50) - 25
+        });
+      }
+    }
+  }
 });
 
 function draw() {
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-	ctx.globalCompositeOperation = "lighter";
+  ctx.globalCompositeOperation = "lighter";
 
-	for (var i = 0, x = stars.length; i < x; i++) {
-		var s = stars[i];
+  for (var i = 0, x = stars.length; i < x; i++) {
+    var s = stars[i];
 
-		ctx.fillStyle = "#fff";
-		ctx.beginPath();
-		ctx.arc(s.x, s.y, s.radius, 0, 2 * Math.PI);
-		ctx.fill();
-		ctx.fillStyle = 'black';
-		ctx.stroke();
-	}
+    ctx.fillStyle = "rgba(255, 255, 255, 0.5)"; // Semi-transparent white
+    ctx.shadowBlur = 20; // Set the shadow blur value
+    ctx.shadowColor = "white"; // Set the shadow color
+    ctx.beginPath();
+    ctx.arc(s.x, s.y, s.radius, 0, 2 * Math.PI);
+    ctx.fill();
+    ctx.fillStyle = 'black';
+    ctx.stroke();
+    ctx.shadowBlur = 0; // Reset shadow blur
+  }
 
-	ctx.beginPath();
-	for (var i = 0, x = stars.length; i < x; i++) {
-		var starI = stars[i];
-		ctx.moveTo(starI.x, starI.y);
-		if (distance(mouse, starI) < 150) ctx.lineTo(mouse.x, mouse.y);
-		for (var j = 0, x = stars.length; j < x; j++) {
-			var starII = stars[j];
-			if (distance(starI, starII) < 150) {
-				//ctx.globalAlpha = (1 / 150 * distance(starI, starII).toFixed(1));
-				ctx.lineTo(starII.x, starII.y);
-			}
-		}
-	}
-	ctx.lineWidth = 0.05;
-	ctx.strokeStyle = 'white';
-	ctx.stroke();
+  ctx.beginPath();
+  for (var i = 0, x = stars.length; i < x; i++) {
+    var starI = stars[i];
+    ctx.moveTo(starI.x, starI.y);
+    if (distance(mouse, starI) < 150) ctx.lineTo(mouse.x, mouse.y);
+    for (var j = 0, x = stars.length; j < x; j++) {
+      var starII = stars[j];
+      if (distance(starI, starII) < 150) {
+        //ctx.globalAlpha = (1 / 150 * distance(starI, starII).toFixed(1));
+        ctx.lineTo(starII.x, starII.y);
+      }
+    }
+  }
+  ctx.lineWidth = 0.05;
+  ctx.strokeStyle = 'white';
+  ctx.stroke();
 }
 
 function distance(point1, point2) {
-	var xs = 0;
-	var ys = 0;
+  var xs = 0;
+  var ys = 0;
 
-	xs = point2.x - point1.x;
-	xs = xs * xs;
+  xs = point2.x - point1.x;
+  xs = xs * xs;
 
-	ys = point2.y - point1.y;
-	ys = ys * ys;
+  ys = point2.y - point1.y;
+  ys = ys * ys;
 
-	return Math.sqrt(xs + ys);
+  return Math.sqrt(xs + ys);
 }
 
-
 function update() {
-	for (var i = 0, x = stars.length; i < x; i++) {
-		var s = stars[i];
+  for (var i = 0, x = stars.length; i < x; i++) {
+    var s = stars[i];
 
-		s.x += s.vx / FPS;
-		s.y += s.vy / FPS;
+    s.x += s.vx / FPS;
+    s.y += s.vy / FPS;
 
-		if (s.x < 0 || s.x > canvas.width) s.vx = -s.vx;
-		if (s.y < 0 || s.y > canvas.height) s.vy = -s.vy;
-	}
+    if (s.x < 0 || s.x > canvas.width) s.vx = -s.vx;
+    if (s.y < 0 || s.y > canvas.height) s.vy = -s.vy;
+  }
 }
 
 function tick() {
-	draw();
-	update();
-	requestAnimationFrame(tick);
+  draw();
+  update();
+  requestAnimationFrame(tick);
 }
 
 tick();
